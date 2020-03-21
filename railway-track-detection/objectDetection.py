1b23229aafe7342e00d11b411293ebb6b775fe43
rishabh kohli <rishabhkohli13@gmail.com>
2020-03-21 23:14:40 +0530

    added object detection to detect moving things(objectDetection.py)

--- railway-track-detection/objectDetection.py
+++ railway-track-detection/objectDetection.py
@@ -1,67 +1,54 @@
-
-import cv2
 import numpy as np
+import cv2
+ 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
 
 
-class RailwayObjectDetection:
-    def __init__(self, ml_rail, cl_rail, mr_rail, cr_rail, focusOfExpansion):
-        self.ml_rail = ml_rail
-        self.cl_rail = cl_rail
-        self.mr_rail = mr_rail
-        self.cr_rail = cr_rail
-        self.foe = focusOfExpansion
+# open webcam video stream
+cap = cv2.VideoCapture("project.mp4")
 
-    # assess the turn markings detection
-    def check_for_turn(self, bbox, bboxold, x, y, x_old, y_old, frame, mask, dynFOEx, pole_points, pole_oldpoints):
+# the output will be written to output.avi
+out = cv2.VideoWriter(
+    'output.avi',
+    cv2.VideoWriter_fourcc(*'MJPG'),
+    15.,
+    (640,480))
 
-        # distance between matched points along x and y
-        distx = np.abs(x - x_old)
-        disty = np.abs(y - y_old)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-        # dynamic FOE variable is initialised as the straight track value, for each feature point considered
-        foe1 = self.foe[0]
+    # resizing for faster detection
+    frame = cv2.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-        # check for keypoints matching goodness
-        for (a, b, w, h) in bbox:
-            for (aold, bold, wold, hold) in bboxold:
-                cv2.rectangle(frame, (a, b), (a + w, b + h), (0, 255, 255), 2)
-                if ((x > a and x < (a + w) and y > b and y < (b + h)) or
-                        (x_old > aold and x_old < (aold + wold) and y_old > bold and y_old < (bold + hold))) and \
-                                distx < 30 and disty < 30:
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
 
-                    cv2.line(mask, (int(x), int(y)), (int(x_old), int(y_old)), (255, 255, 255), 2)
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
 
-                    pole_points.append((x,y))
-                    pole_oldpoints.append((x_old, y_old))
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
+    
+    # Write the output video 
+    out.write(frame.astype('uint8'))
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-                    # significant turn condition
-                    if abs(x - x_old) > 2:
-
-                        # compute which direction the optical flow should have
-                        mt = (float(y - y_old) / (x - x_old + 0.0001)) + 0.0001
-                        ct = (float(y - mt * x))
-
-                        # compute new x coord for focus of expansion FOE
-                        foex = float((self.foe[1] - ct) / mt)
-                        foe1 = foe1 + (foex - foe1)
-
-                        # arrange all FOE x coord in array, making sure that foe1 result is valid
-                        if not np.isnan(foe1):
-                            dynFOEx.append(foe1)
-
-        return dynFOEx, pole_points, pole_oldpoints
-
-    # assess the kilometer signs detection
-    def get_km_features(self, bbox, bboxold, x, y, x_old, y_old, points_km, oldpoints_km):
-        distx = np.abs(x - x_old)
-        disty = np.abs(y - y_old)
-
-        # check for keypoints matching goodness
-        for (a, b, w, h) in bbox:
-            for (aold, bold, wold, hold) in bboxold:
-                if ((x > a and x < (a + w) and y > b and y < (b + h)) or
-                        (x_old > aold and x_old < (aold + wold) and y_old > bold and y_old < (bold + hold))) and \
-                                distx < 30 and distx > 12 and disty < 30 and disty > 8:
-                    points_km.append((float(x),float(y)))
-                    oldpoints_km.append((float(x_old), float(y_old)))
-        return points_km, oldpoints_km
+# When everything done, release the capture
+cap.release()
+# and release the output
+out.release()
+# finally, close the window
+cv2.destroyAllWindows()
+cv2.waitKey(1)
